c1007acfa549e5a194dc5aa4a7f803c59ac9ec53
Roderick Vella Galea <roderick.vella@mcast.edu.mt>
2023-03-09 20:59:22 +0100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# ChatGPT-HTML
+
+- Download your ChatGPT conversation as an html file.
+- Intended primarily for usage by educators so that they could quickly share their inputs and code with their students. For a better learning experience, the html file can be uploaded to Moodle.
+- Code is not obfuscated. No data is being collected.
+
+## Installation
+- Extract project
+- Open Google Chrome and type: chrome://extensions/
+- Enable developers mode
+- Press the 'Load Unpacked' button and select the extracted folder
+
+## Video Demonstration
+[![ChatGPT HTML](https://i.imgur.com/HVfrmpf.png)](https://www.youtube.com/watch?v=cwDL3E7AJs4 "ChatGPT HTML")
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/cwDL3E7AJs4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
---
+++ extension/background.js
@@ -0,0 +1,81 @@
+chrome.browserAction.onClicked.addListener(function (tab) { 
+  if (tab.url.includes("chat.openai.com")) {
+
+    chrome.tabs.executeScript({
+      code: 'document.getElementsByTagName("main")[0].innerHTML;'
+    }, function(result) {
+
+      mainElement = result[0];
+      console.log(mainElement);
+      const imageRegex = /<img\s+[^>]*src=["'][^"']*(image\?url)[^"']*["'][^>]*>/gi;
+      const mainContentsWithoutImages = mainElement.replace(imageRegex, '');
+
+      // Delete buttons with class "p-1"
+      const buttonRegex = /<button\s+[^>]*class=["'][^"']*p-1[^"']*["'][^>]*>[\s\S]*?<\/button>/gi;
+      const mainContentsWithoutButtons = mainContentsWithoutImages.replace(buttonRegex, '');
+
+      // Delete form elements
+      const formRegex = /<form[^>]*>[\s\S]*?<\/form>/gi;
+      const mainContentsWithoutForms = mainContentsWithoutButtons.replace(formRegex, '');
+
+      // Delete buttons with class "cursor-pointer"
+      const cursorPointerButtonRegex = /<button\s+[^>]*class=["'][^"']*cursor-pointer[^"']*["'][^>]*>[\s\S]*?<\/button>/gi;
+      const mainContentsWithoutCursorPointerButtons = mainContentsWithoutForms.replace(cursorPointerButtonRegex, '');
+
+      // Create a new HTML document
+      const newDoc = document.implementation.createHTMLDocument();
+      const htmlElement = newDoc.documentElement;
+      const headElement = newDoc.createElement('head');
+      const bodyElement = newDoc.createElement('body');
+      htmlElement.appendChild(headElement);
+      htmlElement.appendChild(bodyElement);
+
+      // Add stylesheet link and script tag to head element
+      const cssLink = newDoc.createElement('link');
+      cssLink.rel = 'stylesheet';
+      cssLink.href = 'https://cdn.jsdelivr.net/gh/roderickvella/chatgpt_html@style/main.css';
+      headElement.appendChild(cssLink);
+      const tailwindScript = newDoc.createElement('script');
+      tailwindScript.src = 'https://cdn.tailwindcss.com';
+      headElement.appendChild(tailwindScript);
+
+      // Add main contents without cursor-pointer buttons to body element
+      bodyElement.innerHTML = mainContentsWithoutCursorPointerButtons;
+
+      // Add script tag to body element
+      const scriptElement = newDoc.createElement('script');
+      const scriptText = document.createTextNode(`
+  const copyButtons = document.querySelectorAll('button');
+  copyButtons.forEach(button => {
+    if (button.textContent === 'Copy code') {
+      const codeTag = button.parentElement.nextElementSibling.querySelector('code');
+      button.addEventListener('click', () => {
+        const code = codeTag.textContent;
+        navigator.clipboard.writeText(code);
+
+        // Change button text to 'Copied'
+        button.textContent = 'Copied !';
+
+        // Change button text back to 'Copy code' after 2 seconds
+        setTimeout(() => {
+          button.textContent = 'Copy code';
+        }, 2000);
+      });
+    }
+  });
+`);
+      scriptElement.appendChild(scriptText);
+      bodyElement.appendChild(scriptElement);
+
+      // Create a new blob with the HTML content
+      const blob = new Blob([`<!DOCTYPE html>\n${newDoc.documentElement.outerHTML}`], { type: 'text/html' });
+
+      // Create a download link and simulate a click on it
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'file.html';
+      link.click();
+
+    });
+  }
+});
